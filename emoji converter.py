a6f5766c6cb9faf0136214081820f861407a9d8a
ericfly02 <speedyand@gmail.com>
2020-05-30 13:23:35 +0200

    Add files via upload

---
+++ emoji converter.py
@@ -0,0 +1,16 @@
+message = input('>')
+words = message.split(' ')
+emojis = {
+	':)' : '😃',
+
+	':(' : '☹️', 
+
+	'<3' : '❤️', 
+ 
+}
+
+output = ''
+for word in words:
+	output += emojis.get(word, word) + ' '
+
+print (output)
